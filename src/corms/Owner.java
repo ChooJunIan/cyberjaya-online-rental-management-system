6c43b4cef15b90d6a4b96f590e386e3a61a922f8
ChooJunIan <ianchoo2000@gmail.com>
2022-03-21 23:26:04 +0800

    Initial commit

---
+++ src/corms/Owner.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corms;
+
+/**
+ *
+ * @author ianch
+ */
+public class Owner {
+
+    public static Owner _Owner;
+    public int id;
+    public String ownerName;
+    public String ownerPassword;
+    public String ownerPhone;
+}
---
+++ src/corms/Property.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corms;
+
+/**
+ *
+ * @author ianch
+ */
+public class Property {
+
+    public static Property _property;
+    public int propertyID;
+    public String project;
+    public String roadName;
+    public String type;
+    public boolean active;
+    public boolean hasWifi;
+    public boolean hasSP;
+    public int price;
+}
